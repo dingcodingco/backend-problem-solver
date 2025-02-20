61c269c6a376bd8163733e46fdf18b9724f26641
academey <academey@gmail.com>
2025-02-20 23:37:36 +0900

    test commit

--- src/main/java/ding/co/backendportfolio/BackendportfolioApplication.java
+++ src/main/java/ding/co/backendportfolio/BackendportfolioApplication.java
@@ -10,6 +10,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling  // 스케줄링 기능 활성화
 public class BackendportfolioApplication {
     public static void main(String[] args) {
+        System.out.printf("딩코딩코 화이팅3");
         SpringApplication.run(BackendportfolioApplication.class, args);
     }
 
